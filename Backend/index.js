dc140edd1bab2f991013cf2ea852b81674cea374
tanutchasa-tech <tanutcha.sa@ku.th>
2026-03-13 11:35:40 +0700

    add

--- Backend/index.js
+++ Backend/index.js
@@ -1,92 +1,135 @@
 const express = require('express');
-const app = express();
-const port = 8000;
 const bodyParser = require('body-parser');
-const mysql = require('mysql2/promise');
+const mysql = require('mysql2/promise')
+const app = express();
 const cors = require('cors');
 
 app.use(cors());
+
 app.use(bodyParser.json());
 
+const port = 8000;
+
 let conn = null;
-const initMtSQL = async () => {
+const initMySQL = async () => {
     conn = await mysql.createConnection({
         host: 'localhost',
         user: 'root',
         password: 'root',
         database: 'wabdb',
-        port:8700
+        port: 8700
     });
     console.log('Connected to MySQL database');
 }
 
-app.get('/users', async(req,res) => {
+// path: = GET /users สำหรับดึงข้อมูล users ทั้งหมด
+app.get('/users', async (req, res) => {
     const results = await conn.query('SELECT * FROM users');
     res.json(results[0]);
 })
 
-app.post('/users', async(req,res) => {
+const validateData = (userData) => {
+    let errors = [];
+    if (!userData.firstName) {
+        errors.push('กรุณากรอกชื่อ');
+    }
+    if (!userData.lastName) {
+        errors.push('กรุณากรอกนามสกุล');
+    }
+    if (!userData.age) {
+        errors.push('กรุณากรอกอายุ');
+    }
+    if (!userData.gender) {
+        errors.push('กรุณาเลือกเพศ');
+    }
+    if (!userData.interests) {
+        errors.push('กรุณาเลือกงานอดิเรก');
+    }
+    if (!userData.description) {
+        errors.push('กรุณากรอกคำอธิบาย');
+    }
+    return errors;
+}
+
+
+//path: = POST /users สำหรับเพิ่ม user ใหม่
+app.post('/users', async (req, res) => {
     try {
         let user = req.body;
-        const results = await conn.query('INSERT INTO users SET ?',user);
-        console.log('results:',results);
+        const errors = validateData(user);
+        if (errors.length > 0) {
+            throw {
+                message: 'กรุณากรอกข้อมูลให้ครบถ้วน',
+                errors: errors
+            }
+        }
+        const results = await conn.query('INSERT INTO users SET ?', user);
+        console.log('results:', results);
         res.json({
-            massage: 'User added successfully',
+            message: 'User added successfully',
             data: results[0]
         });
-    }catch (error) {
-        console.error('Error inserting user:',error);
-        res.status(500).json({ message: 'Error adding user'});
+    } catch (error) {
+        const errorMessage = error.message || 'Error adding user';
+        const errors = error.errors || [];
+        console.error('Error inserting user:', error);
+        res.status(500).json({ 
+            message: errorMessage,
+            errors: errors
+        });
     }
 })
 
-app.get('/users/:id', async (req,res) => {
+//path: = GET /users/:id สำหรับดึงข้อมูล user ตาม id
+app.get('/users/:id', async (req, res) => {
     try {
         let id = req.params.id;
-        const results = await conn.query('SELECT *FROM users WHERE id = ?',id);
+        const results = await conn.query('SELECT * FROM users WHERE id = ?', id);
         if (results[0].length === 0) {
-            throw {statusCode: 404, message: 'User not found'};
+            throw { statusCode: 404, message: 'User not found' };
         }
         res.json(results[0][0]);
     } catch (error) {
-        console.error('Error fetching user:',error);
+        console.error('Error fetching user:', error);
         let statusCode = error.statusCode || 500;
-        res.status(statusCode).json({ 
-            message: error.massage || 'Error fetching user'});
+        res.status(statusCode).json({
+            message: error.message || 'Error fetching user'
+        });
     }
 })
 
-//Path: put
-app.put('/users/:id', async (req,res) => {
-    try{
+//path: = PUT /users/:id สำหรับอัพเดทข้อมูล user ตาม id
+app.put('/users/:id', async (req, res) => {
+    try {
         let id = req.params.id;
         let updateUser = req.body;
         const results = await conn.query('UPDATE users SET ? WHERE id = ?', [updateUser, id]);
         res.json({
-            massage: 'User updated successfully',
+            message: 'User updated successfully',
             data: results[0]
         });
     } catch (error) {
-        console.error('Error updating user:',error);
-        res.status(500).json({ message: 'Error updating user'});
+        console.error('Error updating user:', error);
+        res.status(500).json({ message: 'Error updating user' });
     }
 })
 
-app.delete('/users/:id', async (req,res) => {
-    try{
+//path: = DELETE /users/:id สำหรับลบ user ตาม id
+app.delete('/users/:id', async (req, res) => {
+    try {
         let id = req.params.id;
-        const results = await conn.query('DELETE FROM users WHERE id = ?',[id]);
-        res.json({
-            massage: 'User deleted successfully',
+        const results = await conn.query('DELETE FROM users WHERE id = ?', id);
+        res.json({  
+            message: 'User deleted successfully',
             data: results[0]
         });
     } catch (error) {
-        console.error('Error deleting user:',error);
-        res.status(500).json({ message: 'Error deleting user'});
+        console.error('Error deleting user:', error);
+        res.status(500).json({ message: 'Error deleting user' });
     }
 })
 
 app.listen(port, async () => {
-    await initMtSQL();
-    console.log(`server is running on http://localhost:${port}`);
+    await initMySQL();
+    console.log(`Server is running on http://localhost:${port}`);
 });
--- Frontend/index.js
+++ Frontend/index.js
@@ -21,7 +21,6 @@ const validateData = (userData) => {
     return errors;
 }
 
-
 const submitData = async () => {
     let firstNameDOM = document.querySelector('input[name=firstname]');
     let lastNameDOM = document.querySelector('input[name=lastname]');
@@ -30,55 +29,59 @@ const submitData = async () => {
     let interestDOMs = document.querySelectorAll('input[name=interests]:checked') || {};
     let descriptionDOM = document.querySelector('textarea[name=description]');
 
-    let messageDom = document.getElementById('message')
+    let messageDOM = document.getElementById('message')
     try {
-    let interest = ''
+        let interest = ''
         for (let i = 0; i < interestDOMs.length; i++) {
-        interest += interestDOMs[i].value
-        if (i != interestDOMs.length - 1) {
-            interest += ','
+            interest += interestDOMs[i].value
+            if (i != interestDOMs.length - 1) {
+                interest += ','
+            }
         }
-    }
 
-    let userData ={
-        firstName: firstNameDOM.value,
-        lastName: lastNameDOM.value,
-        age: ageDOM.value,
-        gender: genderDOM.value,
-        description: descriptionDOM.value,
-        interests: interest
-    }
-    console.log('submitData', userData);
-
-    const errors = validateData(userData);
-    if (errors.length > 0){
-        throw {
-            message: 'กรุณากรอกข้อมูลให้ครบถ้วน',
-            errors: errors
+        let userData = {
+            firstName: firstNameDOM.value,
+            lastName: lastNameDOM.value,
+            age: ageDOM.value,
+            gender: genderDOM.value,
+            description: descriptionDOM.value,
+            interests: interest
         }
-    }
+        console.log('submitData', userData);
+
+        const errors = validateData(userData);
+        if (errors.length > 0) {
+            throw {
+                message: 'กรุณากรอกข้อมูลให้ครบถ้วน',
+                errors: errors
+            }
+        }
 
         const response = await axios.post('http://localhost:8000/users', userData);
-        console.log('response',response);
-        messageDom.innerText = 'บันทึกข้อมูลสำเร็จ';
-        messageDom.className = 'message success';
-    }catch (error) {
-        console.log('error message',error.message);
-        console.log('error details',error.error);
-        //if (error.response) {
-       //     console.error('Error response:', error.response.data.message);
-       // }
-       let htmlData = '<div>'
-       htmlData += `<div>${error.message}</div>`;
-       htmlData += '<ul>';
-       for (let i = 0; i < error.errors.length; i++){
-        htmlData += `<li>${error.errors[i]}</li>`;
-       }
-       htmlData += '</ul>'
-       htmlData += '</div>';
+        console.log('response', response);
+        messageDOM.innerText = 'บันทึกข้อมูลสำเร็จ';
+        messageDOM.className = 'message success';
+    } catch (error) {
+        console.log('error message', error.message);
+        console.log('error', error.errors);
 
-        messageDom.innerHTML = htmlData;
-        messageDom.className = 'message danger';
+        if (error.response) {
+            console.log('Error response:', error.response);
+            error.message = error.response.data.message
+            error.errors = error.response.data.errors
+        }
+
+        let htmlData = '<div>'
+        htmlData += `<div>${error.message}</div>`;
+        htmlData += '<ul>';
+        for (let i = 0; i < error.errors.length; i++) {
+            htmlData += `<li>${error.errors[i]}</li>`;
+        }
+        htmlData += '</ul>';
+        htmlData += '</div>';
+
+
+        messageDOM.innerHTML = htmlData;
+        messageDOM.className = 'message danger';
     }
-     
 }
